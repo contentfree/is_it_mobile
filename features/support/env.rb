6d1d51ef23daa1db409063de4ae53c7e3bb20428
Dave Myron <dave.myron@contentfree.com>
2009-04-24 13:23:54 -0700

    Initial commit to is_it_mobile.

---
+++ features/support/env.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'is_it_mobile'
+
+require 'spec/expectations'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'is_it_mobile'
+
+Spec::Runner.configure do |config|
+  
+end
